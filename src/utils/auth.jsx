3e3b699af02447f8bac0dba7324e87a3ce34a39a
Murat Karadeniz <murat.k>
2024-05-17 16:15:54 +0300

    Auth response message update

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -55,21 +55,21 @@ export const register = async (nameSurname, username, email, password) => {
     if (response.status === 201) {
       const token = response.data.data;
       localStorage.setItem('registerToken', JSON.stringify(token));
-      return { success: true, message: 'Kayıt başarılı', token };
+      return { success: true, message: 'Registration successful', token };
     }
   } catch (error) {
     if (error.response) {
       console.error('Register error:', error.response.data);
       return {
         success: false,
-        message: error.response.data.message || 'Kayıt başarısız',
+        message: error.response.data.message || 'Registration failed',
       };
     } else if (error.request) {
       console.error('Register error:', error.request);
-      return { success: false, message: 'Sunucudan yanıt yok' };
+      return { success: false, message: 'No response from server' };
     } else {
       console.error('Register error:', error.message);
-      return { success: false, message: 'İstek hatası' };
+      return { success: false, message: 'Request error' };
     }
   }
 };
@@ -88,21 +88,21 @@ export const confirmMail = async (verificationCode) => {
 
     if (response.status === 200) {
       localStorage.removeItem('registerToken');
-      return { success: true, message: 'E-posta doğrulama başarılı' };
+      return { success: true, message: 'Email verification successful' };
     }
   } catch (error) {
     if (error.response) {
       console.error('Confirm mail error:', error.response.data);
       return {
         success: false,
-        message: error.response.data.message || 'E-posta doğrulama başarısız',
+        message: error.response.data.message || 'Email verification failed',
       };
     } else if (error.request) {
       console.error('Confirm mail error:', error.request);
-      return { success: false, message: 'Sunucudan yanıt yok' };
+      return { success: false, message: 'No response from server' };
     } else {
       console.error('Confirm mail error:', error.message);
-      return { success: false, message: 'İstek hatası' };
+      return { success: false, message: 'Request error' };
     }
   }
 };
