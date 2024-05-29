29744289d416447bf6c856f89e158bb68aa9ebd9
Murat Karadeniz <muratkaradeniz800@gmail.com>
2024-05-29 23:11:40 +0300

    Update matchinfo
    players have been formated to lineup

--- src/components/MatchesInfo.jsx
+++ src/components/MatchesInfo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getMatchDetail } from '../api/matchApi'
 import { CgSpinner } from 'react-icons/cg'
 import { formatDateAndTime } from '../utils/dateUtils'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { FaFutbol } from 'react-icons/fa6'
 import real_madrid from '/real_madrid.png'
 
@@ -53,11 +53,63 @@ function MatchesInfo() {
     return member ? { shirtNumber: member.shirtNumber , mainPosition: member.mainPosition, altPosition: member.altPosition.abbreviation } : { shirtNumber:null, mainPosition: null, altPosition: null };
   };
 
-  
-  
+  const positionLabels = {
+    '663e94ae3f58cf748889aaed': 'Forvet',
+    '663e94ae3f58cf748889aaca': 'Orta Saha',
+    '663e94ae3f58cf748889aa98': 'Defans',
+    '663e94ae3f58cf748889ab10': 'Kaleci'
+  };
+
+  const organizePlayersByPosition = (players) => {
+    const positions = {
+      '663e94ae3f58cf748889aaed': [], // Forvet
+      '663e94ae3f58cf748889aaca': [], // Orta Saha
+      '663e94ae3f58cf748889aa98': [], // Defans
+      '663e94ae3f58cf748889ab10': []  // Kaleci
+    };
+
+    players.forEach(player => {
+      const { mainPosition } = getPlayerPositions(player.user._id);
+      if (positions[mainPosition]) {
+        positions[mainPosition].push(player);
+      }
+    });
+
+    return positions;
+  };
+
+  const homePlayersByPosition = organizePlayersByPosition(homeTeamPlayers);
+  const awayPlayersByPosition = organizePlayersByPosition(awayTeamPlayers);
+
+  const renderPlayers = (players) => {
+    return players.map((player) => {
+      const positions = getPlayerPositions(player.user._id);
+      return (
+        <div
+          key={player.user._id}
+          className="flex h-16 md:h-20 bg-background-theme bg-cover line-clamp-1 truncate bg-center rounded-xl cursor-pointer w-54 md:w-72"
+        >
+          <div className="flex items-center mx-2 md:mx-4 min-w-12">
+            <div className="relative text-center content-center bg-green-600 h-10 w-10 md:h-12 md:w-12 rounded-full">
+              <p className="font-medium md:text-md">10.0</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center space-y-1 min-w-0">
+            <h1 className="text-md md:text-lg font-medium truncate">
+              {player.user.nameSurname}
+            </h1>
+            <h3 className="text-sm font-medium text-gray-300 truncate">
+              # {positions.shirtNumber} - {positionLabels[positions.mainPosition]}
+            </h3>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="pt-8">
-      <div className="flex">
+      <div className="flex justify-center">
         <div className="flex items-center mx-4 md:mx-10 min-w-14">
           <img className="object-fit h-20" src={real_madrid} alt="Real Madrid Logo" />
         </div>
@@ -80,56 +132,38 @@ function MatchesInfo() {
             <FaFutbol />
             <p>{homeTeamPlayers.length + awayTeamPlayers.length}</p>
           </div>
-          <div className="grid md:grid-cols-2 gap-4">
-            {homeTeamPlayers.map((player) => {
-              const positions = getPlayerPositions(player.user._id);
-              return (
-                <div
-                  key={player.user._id}
-                  className="flex h-24 md:h-32 bg-background-theme bg-cover line-clamp-1 truncate bg-center rounded-xl cursor-pointer"
-                >
-                  <div className="flex items-center mx-5 md:mx-10 min-w-16">
-                    <div className="relative text-center content-center bg-green-600 h-14 w-14 md:h-16 md:w-16 rounded-full">
-                      <p className="font-medium md:text-lg">10.0</p>
-                    </div>
-                  </div>
-                  <div className="flex flex-col justify-center space-y-1 min-w-0">
-                    <h1 className="text-lg md:text-xl font-medium truncate">
-                      {player.user.nameSurname}
-                    </h1>
-                    <h3 className="text-lg font-medium text-gray-300 truncate">
-                      # {positions.shirtNumber} - {positions.mainPosition}
-                    </h3>
-                  </div>
-                </div>
-              );
-            })}
-            {awayTeamPlayers.map((player) => (
-              <div
-                key={player.user._id}
-                className="flex h-24 md:h-32 bg-background-theme bg-cover line-clamp-1 truncate bg-center rounded-xl cursor-pointer"
-              >
-                <div className="flex items-center mx-5 md:mx-10 min-w-16">
-                  <div className="relative text-center content-center bg-green-600 h-14 w-14 md:h-16 md:w-16 rounded-full">
-                    <p className="font-medium md:text-lg">10.0</p>
-                  </div>
-                </div>
-                <div className="flex flex-col justify-center space-y-1 min-w-0">
-                  <h1 className="text-lg md:text-xl font-medium truncate">
-                    {player.user.nameSurname}
-                  </h1>
-                  <h3 className="text-lg font-medium text-gray-300 truncate">
-                    #{player.user.nameSurname} - {player.mainPosition} -{' '}
-                    {player.altPosition}
-                  </h3>
-                </div>
-              </div>
-            ))}
+          <div className="space-y-4">
+            <div className="flex justify-center">
+
+            </div>
+            <div className="flex justify-center flex-wrap gap-4">
+              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aaed'])}
+              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aaed'])}
+            </div>
+            <div className="flex justify-center">
+
+            </div>
+            <div className="flex justify-center flex-wrap gap-4">
+              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aaca'])}
+              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aaca'])}
+            </div>
+            <div className="flex justify-center">
+
+            </div>
+            <div className="flex justify-center flex-wrap gap-4">
+              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aa98'])}
+              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aa98'])}
+            </div>
+            <div className="flex justify-center">
+
+            </div>
+            <div className="flex justify-center flex-wrap gap-4">
+              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889ab10'])}
+              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889ab10'])}
+            </div>
           </div>
         </div>
       </div>
-
-
     </div>
   )
 }
