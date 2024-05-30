a9f82bae9d2e23dcee1768134604be0a85117bf0
Eren Yavaş <eerenyavas@gmail.com>
2024-05-30 22:40:56 +0300

    matches added, line up edited on match details

--- src/components/MatchesInfo.jsx
+++ src/components/MatchesInfo.jsx
@@ -16,7 +16,6 @@ function MatchesInfo() {
     const fetchMatchDetail = async () => {
       try {
         const response = await getMatchDetail(id)
-
         if (response.success === true) {
           setMatchDetail(response.data.data)
         } else if (response.success === false) {
@@ -48,70 +47,36 @@ function MatchesInfo() {
   const homeTeamPlayers = matchDetail.lineup.homeTeam || []
   const awayTeamPlayers = matchDetail.lineup.awayTeam || []
 
-  const getPlayerPositions = (userId) => {
-    const member = matchDetail.createdGroupId.members.find(member => member.user === userId);
-    return member ? { shirtNumber: member.shirtNumber , mainPosition: member.mainPosition, altPosition: member.altPosition.abbreviation } : { shirtNumber:null, mainPosition: null, altPosition: null };
-  };
-
-  const positionLabels = {
-    '663e94ae3f58cf748889aaed': 'Forvet',
-    '663e94ae3f58cf748889aaca': 'Orta Saha',
-    '663e94ae3f58cf748889aa98': 'Defans',
-    '663e94ae3f58cf748889ab10': 'Kaleci'
-  };
-
   const organizePlayersByPosition = (players) => {
     const positions = {
-      '663e94ae3f58cf748889aaed': [], // Forvet
-      '663e94ae3f58cf748889aaca': [], // Orta Saha
-      '663e94ae3f58cf748889aa98': [], // Defans
-      '663e94ae3f58cf748889ab10': []  // Kaleci
-    };
+      FWD: [], // Forvet
+      MID: [], // Orta Saha
+      DEF: [], // Defans
+      GK: [], // Kaleci
+    }
 
-    players.forEach(player => {
-      const { mainPosition } = getPlayerPositions(player.user._id);
-      if (positions[mainPosition]) {
-        positions[mainPosition].push(player);
+    players.forEach((player) => {
+      const { abbreviation } = player.position
+      if (positions[abbreviation]) {
+        positions[abbreviation].push(player)
       }
-    });
+    })
 
-    return positions;
-  };
+    return positions
+  }
 
-  const homePlayersByPosition = organizePlayersByPosition(homeTeamPlayers);
-  const awayPlayersByPosition = organizePlayersByPosition(awayTeamPlayers);
-
-  const renderPlayers = (players) => {
-    return players.map((player) => {
-      const positions = getPlayerPositions(player.user._id);
-      return (
-        <div
-          key={player.user._id}
-          className="flex h-16 md:h-20 bg-background-theme bg-cover line-clamp-1 truncate bg-center rounded-xl cursor-pointer w-54 md:w-72"
-        >
-          <div className="flex items-center mx-2 md:mx-4 min-w-12">
-            <div className="relative text-center content-center bg-green-600 h-10 w-10 md:h-12 md:w-12 rounded-full">
-              <p className="font-medium md:text-md">10.0</p>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center space-y-1 min-w-0">
-            <h1 className="text-md md:text-lg font-medium truncate">
-              {player.user.nameSurname}
-            </h1>
-            <h3 className="text-sm font-medium text-gray-300 truncate">
-              # {positions.shirtNumber} - {positionLabels[positions.mainPosition]}
-            </h3>
-          </div>
-        </div>
-      );
-    });
-  };
+  const homePlayersByPosition = organizePlayersByPosition(homeTeamPlayers)
+  const awayPlayersByPosition = organizePlayersByPosition(awayTeamPlayers)
 
   return (
     <div className="pt-8">
       <div className="flex justify-center">
         <div className="flex items-center mx-4 md:mx-10 min-w-14">
-          <img className="object-fit h-20" src={real_madrid} alt="Real Madrid Logo" />
+          <img
+            className="object-fit h-20"
+            src={real_madrid}
+            alt="Real Madrid Logo"
+          />
         </div>
         <div className="flex flex-col justify-center space-y-1 min-w-0">
           <h1 className="text-lg md:text-xl font-medium truncate">
@@ -123,7 +88,7 @@ function MatchesInfo() {
         </div>
       </div>
       <div className="p-4">
-        <div>
+        <div className="">
           <h1 className="text-lg">Maç Detayı</h1>
         </div>
         <div className="space-y-4">
@@ -132,34 +97,62 @@ function MatchesInfo() {
             <FaFutbol />
             <p>{homeTeamPlayers.length + awayTeamPlayers.length}</p>
           </div>
-          <div className="space-y-4">
-            <div className="flex justify-center">
-
-            </div>
-            <div className="flex justify-center flex-wrap gap-4">
-              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aaed'])}
-              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aaed'])}
-            </div>
-            <div className="flex justify-center">
-
-            </div>
-            <div className="flex justify-center flex-wrap gap-4">
-              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aaca'])}
-              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aaca'])}
-            </div>
-            <div className="flex justify-center">
-
-            </div>
-            <div className="flex justify-center flex-wrap gap-4">
-              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889aa98'])}
-              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889aa98'])}
-            </div>
-            <div className="flex justify-center">
-
-            </div>
-            <div className="flex justify-center flex-wrap gap-4">
-              {renderPlayers(homePlayersByPosition['663e94ae3f58cf748889ab10'])}
-              {renderPlayers(awayPlayersByPosition['663e94ae3f58cf748889ab10'])}
+          <div className="flex justify-center ">
+            <div className="h-[500px] bg-green-soccer-field-theme w-1/3 bg-contain bg-no-repeat">
+              <div className="space-y-12 pt-28">
+                <div className="flex justify-around ">
+                  {homePlayersByPosition.FWD.map((player) => {
+                    return (
+                      <div key={player} className="">
+                        <div className="relative text-center content-center bg-gray-700 h-10 w-10 md:h-12 md:w-12 rounded-full border-gray-400 border-4">
+                          <p className="font-semibold md:text-md">
+                            {player.user.shirtNumber}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div className="flex justify-around ">
+                  {homePlayersByPosition.MID.map((player) => {
+                    return (
+                      <div key={player} className="">
+                        <div className="relative text-center content-center bg-gray-700 h-10 w-10 md:h-12 md:w-12 rounded-full border-gray-400 border-4">
+                          <p className="font-semibold md:text-md">
+                            {player.user.shirtNumber}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div className="flex  justify-around ">
+                  {homePlayersByPosition.DEF.map((player) => {
+                    return (
+                      <div key={player} className="">
+                        <div className="relative text-center content-center bg-gray-700 h-10 w-10 md:h-12 md:w-12 rounded-full border-gray-400 border-4">
+                          <p className="font-semibold md:text-md">
+                            {player.user.shirtNumber}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div className="flex justify-center">
+                  {homePlayersByPosition.GK.map((player) => {
+                    return (
+                      <div key={player} className="">
+                        <div className="relative text-center content-center bg-gray-700 h-10 w-10 md:h-12 md:w-12 rounded-full border-gray-400 border-4">
+                          <p className="font-semibold md:text-md">
+                            {player.user.shirtNumber}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import MatchesAll from '../components/MatchesAll'
+import { getMatches } from '../api/matchApi'
 
 function Matches() {
   return (
-    <div className="lg:px-20 xl:px-60">
-      <MatchesAll />
+    <div className="lg:px-20 xl:px-60 py-8">
+      <MatchesAll fetchDataMethod={getMatches()} isGroupBy={false} />
     </div>
   )
 }
